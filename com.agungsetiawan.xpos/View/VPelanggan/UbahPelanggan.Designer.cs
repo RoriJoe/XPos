27210f65bd8fb8725dc8d664fd44f4695fc5d050
agung_setiawan <ag.setiawan@id.jmtech.co.jp>
2015-04-06 15:43:57 +0900

    fix label "kategori" jadi "jenis member" di form tambah dan ubah pelanggan

--- com.agungsetiawan.xpos/View/VPelanggan/UbahPelanggan.Designer.cs
+++ com.agungsetiawan.xpos/View/VPelanggan/UbahPelanggan.Designer.cs
@@ -147,9 +147,9 @@
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.Location = new System.Drawing.Point(11, 219);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(53, 15);
+            this.label7.Size = new System.Drawing.Size(86, 15);
             this.label7.TabIndex = 37;
-            this.label7.Text = "Kategori";
+            this.label7.Text = "Jenis Member";
             // 
             // label4
             // 
